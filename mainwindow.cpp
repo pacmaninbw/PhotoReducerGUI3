2414f33a7905ecaa3383161ebcd512bc3a85c271
pacmaninbw <paul.chernick@chernicksw.com>
2025-01-23 08:36:18 -0800

    Added source and target directory display.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,21 +1,12 @@
+#include <QDir>
 #include "mainwindow.h"
 #include "OptionsDialog.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-    constexpr int mainWindowWidth = 800;
-    constexpr int mainWindowHeight = 661;
 
-    resize(mainWindowWidth, mainWindowHeight);
-
-    centralwidget = new QWidget(this);
-    centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
-
-    optionsPushButton = new QPushButton("Options", centralwidget);
-    optionsPushButton->setObjectName("optionsPushButton");
-
-    setCentralWidget(centralwidget);
+    setUpUI();
 
     connect(optionsPushButton, &QPushButton::clicked, this, &MainWindow::on_optionsPushButton_Clicked);
 }
@@ -24,20 +15,9 @@ MainWindow::~MainWindow()
 {
 }
 
-QLCDNumber *MainWindow::createAndConfigureLCD(const char *lcdName)
-{
-    QLCDNumber* lcd = new QLCDNumber;
-
-    QPalette palette = lcd->palette();
-    palette.setColor(QPalette::Window, Qt::black); // Set background color to black
-    palette.setColor(QPalette::WindowText, Qt::green); // Set text color to green
-    lcd->setAutoFillBackground(true);
-    lcd->setPalette(palette);
-    lcd->setSegmentStyle(QLCDNumber::Flat);
-
-    return lcd;
-}
-
+/*
+ * Slots
+ */
 void MainWindow::on_optionsPushButton_Clicked()
 {
     OptionsDialog optionDialog(this);
@@ -51,3 +31,87 @@ void MainWindow::on_optionsPushButton_Clicked()
     }
 }
 
+/*
+ * Private UI methods
+ */
+void MainWindow::setUpUI()
+{
+    resize(mainWindowWidth, mainWindowHeight);
+
+    centralwidget = new QWidget(this);
+    centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+
+    optionsPushButton = new QPushButton("Options", centralwidget);
+    optionsPushButton->setObjectName("optionsPushButton");
+    optionsPushButton->setGeometry(calculateHorizontalCenteredObjectRect(optionsPushButton->width(), buttonHeight));
+    currentHeight += buttonHeight + verticleSpace;
+
+    setUpDirectoryDisplays();
+
+    setCentralWidget(centralwidget);
+}
+
+void MainWindow::setUpDirectoryDisplays()
+{
+    sourceDirectoryLabel = createCenteredLabel("Source Directory", "sourceDirectoryLabel");
+    sourceDirectoryValueLabel = createDirectoryDisplayLab("sourceDirectoryValueLabel");
+
+    targetDirectoryLabel = createCenteredLabel("Target Directory", "targetDirectoryLabel");
+    targetDirectoryValueLabel = createDirectoryDisplayLab("targetDirectoryValueLabel");
+}
+
+QLCDNumber *MainWindow::createAndConfigureLCD(const char *lcdName)
+{
+    QLCDNumber* lcd = new QLCDNumber;
+
+    lcd->setObjectName(QString::fromUtf8(lcdName));
+
+    QPalette palette = lcd->palette();
+    palette.setColor(QPalette::Window, Qt::black); // Set background color to black
+    palette.setColor(QPalette::WindowText, Qt::green); // Set text color to green
+    lcd->setAutoFillBackground(true);
+    lcd->setPalette(palette);
+    lcd->setSegmentStyle(QLCDNumber::Flat);
+
+    return lcd;
+}
+
+QRect MainWindow::calculateHorizontalCenteredObjectRect(int objectWidth, int objectHeigth)
+{
+    int offsetBase = static_cast<int>(objectWidth / 2);
+    int leftX = mainWindowHorizontalCenter - offsetBase;
+    int topY = currentHeight;
+
+    return QRect(leftX, topY, objectWidth, objectHeigth);
+}
+
+int MainWindow::getLabelWidth(QLabel *lab)
+{
+    QFont currentFont = lab->font();
+    QFontMetrics fm(currentFont);
+    return fm.horizontalAdvance(lab->text());
+}
+
+QLabel *MainWindow::createCenteredLabel(const char *labText, const char *labName)
+{
+    QLabel* newLabel;
+
+    newLabel = new QLabel(labText, centralwidget);
+    newLabel->setObjectName(QString::fromUtf8(labName));
+    newLabel->setGeometry(calculateHorizontalCenteredObjectRect(
+        getLabelWidth(newLabel), labelHeight));
+    currentHeight += labelHeight + verticleSpace;
+
+    return newLabel;
+}
+
+QLabel *MainWindow::createDirectoryDisplayLab(const char *labName)
+{
+    QLabel* newDisplay = new QLabel(centralwidget);
+    newDisplay->setObjectName(QString::fromUtf8(labName));
+    newDisplay->setGeometry(calculateHorizontalCenteredObjectRect(maxOjectWidth, labelHeight));
+    newDisplay->setFrameShape(QFrame::Box);
+    currentHeight += labelHeight + verticleSpace;
+
+    return newDisplay;
+}
--- mainwindow.h
+++ mainwindow.h
@@ -8,6 +8,7 @@
 #include <QMainWindow>
 #include <QMenuBar>
 #include <QPushButton>
+#include <QRect>
 #include <QStatusBar>
 #include <QWidget>
 
@@ -28,7 +29,26 @@ private slots:
     void on_optionsPushButton_Clicked();
 
 private:
+/*
+ * Size and positioning constants.
+ */
+    const int mainWindowWidth = 800;
+    const int maxOjectWidth = static_cast<int>(mainWindowWidth * 0.8);
+    const int mainWindowHorizontalCenter = static_cast<int>(mainWindowWidth / 2);
+    const int mainWindowHeight = 661;
+    const int lcdHeight = 23;
+    const int labelHeight = 17;
+    const int verticleSpace = 20;
+    const int buttonHeight = 25;
+
+    void setUpUI();
+    void setUpDirectoryDisplays();
     QLCDNumber* createAndConfigureLCD(const char* lcdName);
+    QRect calculateHorizontalCenteredObjectRect(int objectWidth, int objectHeigth);
+    int getLabelWidth(QLabel* lab);
+    QLabel* createCenteredLabel(const char *labText, const char* labName);
+    QLabel* createDirectoryDisplayLab(const char* labName);
+
     QWidget *centralwidget;
     QLCDNumber *filesToResizeLcdNumber;
     QLabel *filesToResizeLabel;
@@ -42,7 +62,8 @@ private:
     QPushButton *resizePhotosButton;
     QMenuBar *menubar;
     QStatusBar *statusbar;
-
+    int currentHeight = 20;
+    int itemLineCount = 0;
 };
 #endif // MAINWINDOW_H_
 
