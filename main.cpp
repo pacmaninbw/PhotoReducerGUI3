6c9a683e08cbc200c01f7b6d4d01d6558fe7357b
pacmaninbw <paul.chernick@chernicksw.com>
2025-01-26 12:23:21 -0800

    Added the model to main.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "mainwindow.h"
+#include "PhotoReducerModel.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication theApp(argc, argv);
+
+    PhotoReducerModel theModel;
 
     MainWindow mainWindow;
     mainWindow.show();
